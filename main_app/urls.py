9fb771266fe3d5d266bb8c79e779fd025a51253b
Carolina <carolinaurrea_8@hotmail.com>
2021-06-08 17:53:10 -0500

    Trying things out

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,16 +6,12 @@ from django.urls import path, include
 from rest_framework import routers
 from main_app import views
 from django.conf import settings
-from django.contrib.auth.views import logout
+# from django.contrib.auth.views import logout
 
 router = routers.DefaultRouter()
 router.register(r'products', views.ProductView, 'product')
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('api/',  include(router.urls)),
-    path('', include('social_django.urls', namespace='social')),
-    path('logout/', logout, {'next_page': settings.LOGOUT_REDIRECT_URL},
-         name='logout'),
-    # path("/", include('main_app.urls)
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
